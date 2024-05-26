1dd1f7d04c6a4ffbf4b84128fab2e8f3fc9de098
jkdev <gpluiy@gmail.com>
2024-05-26 17:14:03 +0200

    MapMaker game mode added

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -2,6 +2,7 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using Newtonsoft.Json.Linq;
+using Polytopia.Data;
 using UnityEngine;
 
 namespace PolyMod
@@ -22,8 +23,9 @@ namespace PolyMod
 #pragma warning restore CS8618
 		internal static int version = -1;
 		internal static bool start = false;
+        internal static bool mapMakerSettedUp = false;
 
-		[HarmonyPostfix]
+        [HarmonyPostfix]
 		[HarmonyPatch(typeof(GameManager), nameof(GameManager.Update))]
 		private static void GameManager_Update()
 		{
@@ -34,17 +36,20 @@ namespace PolyMod
 		{
 			Harmony.CreateAndPatchAll(typeof(Plugin));
 			Harmony.CreateAndPatchAll(typeof(BotGame));
-			Harmony.CreateAndPatchAll(typeof(MapLoader));
-			Harmony.CreateAndPatchAll(typeof(ModLoader));
+			Harmony.CreateAndPatchAll(typeof(MapManager));
+            EnumCache<ImprovementAbility.Type>.AddMapping("climatesetter", (ImprovementAbility.Type)600);
+            EnumCache<ImprovementAbility.Type>.AddMapping("climatesetter", (ImprovementAbility.Type)600);
+            Harmony.CreateAndPatchAll(typeof(ModLoader));
 			Harmony.CreateAndPatchAll(typeof(UI));
 			logger = Log;
-		}
+            Harmony.CreateAndPatchAll(typeof(Jkdev));
+        }
 
 		internal static void Start()
 		{
 			BotGame.Init();
-			MapLoader.Init();
-		}
+			MapManager.Init();
+        }
 
 		internal static void Update()
 		{
@@ -58,7 +63,20 @@ namespace PolyMod
 			{
 				UI.Show();
 			}
-		}
+
+			if (GameManager.Instance.isLevelLoaded && MapManager.isInMapMaker && !mapMakerSettedUp)
+			{
+                HudButtonBar buttonBar = (UIManager.Instance.GetScreen(UIConstants.Screens.Hud) as HudScreen).buttonBar;
+                buttonBar.blockRefreshingNextTurnButton = true;
+                buttonBar.blockRefreshingStatsButton = true;
+                buttonBar.blockRefreshingTechTreeButton = true;
+                buttonBar.positionDisplay.gameObject.SetActive(false);
+                buttonBar.nextTurnButton.BlockButton = true;
+                buttonBar.techTreeButton.BlockButton = true;
+                buttonBar.statsButton.BlockButton = true;
+				mapMakerSettedUp = true;
+            }
+        }
 
 		internal static string GetJTokenName(JToken token, int n = 1)
 		{
