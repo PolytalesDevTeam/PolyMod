06beab314fd4049ede9b1256bcb8a1c12e38ccbd
IExploitableMan <pythonblack5000@gmail.com>
2024-09-07 13:28:56 +0300

    Cleanup

--- src/ModLoader.cs
+++ src/ModLoader.cs
@@ -8,7 +8,6 @@ using Polytopia.Data;
 using System.IO.Compression;
 using System.Reflection;
 using UnityEngine;
-using UnityEngine.InputSystem.Interactions;
 
 namespace PolyMod
 {
@@ -16,10 +15,9 @@ namespace PolyMod
 	{
 		private static int _autoidx = Plugin.AUTOIDX_STARTS_FROM;
 		private static List<JObject> _patches = new();
-		private static Dictionary<string, byte[]> _textures = new();
 		private static Dictionary<string, Sprite> _sprites = new();
 		private static Dictionary<string, AudioClip> _audios = new();
-		public static Dictionary<string, int> gldDictionary = new ();
+		public static Dictionary<string, int> gldDictionary = new();
 
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(GameLogicData), nameof(GameLogicData.AddGameLogicPlaceholders))]
@@ -33,135 +31,6 @@ namespace PolyMod
 		private static void PurchaseManager_IsTribeUnlocked(ref bool __result, TribeData.Type type)
 		{
 			__result = (int)type >= Plugin.AUTOIDX_STARTS_FROM || __result;
-		}
-
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetTileSpriteAddress), new Type[] { typeof(Polytopia.Data.TerrainData.Type), typeof(string) })]
-		private static void SpriteData_GetTileSpriteAddress(ref SpriteAddress __result, Polytopia.Data.TerrainData.Type terrain, string skinId)
-		{
-			__result = GetSprite(__result, EnumCache<Polytopia.Data.TerrainData.Type>.GetName(terrain), skinId);
-		}
-
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetResourceSpriteAddress), new Type[] { typeof(ResourceData.Type), typeof(string) })]
-		private static void SpriteData_GetResourceSpriteAddress(ref SpriteAddress __result, ResourceData.Type type, string skinOrTribeAsString)
-		{
-			__result = GetSprite(__result, EnumCache<ResourceData.Type>.GetName(type), skinOrTribeAsString);
-		}
-
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetBuildingSpriteAddress), new Type[] { typeof(ImprovementData.Type), typeof(string) })]
-		private static void SpriteData_GetBuildingSpriteAddress(ref SpriteAddress __result, ImprovementData.Type type, string climateOrSkinAsString)
-		{
-			__result = GetSprite(__result, EnumCache<ImprovementData.Type>.GetName(type), climateOrSkinAsString);
-		}
-
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetUnitIconAddress))]
-		private static void SpriteData_GetUnitIconAddress(ref SpriteAddress __result, UnitData.Type type)
-		{
-			__result = GetSprite(__result, "icon", EnumCache<UnitData.Type>.GetName(type));
-		}
-
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(SkinType) })]
-		private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, SkinType skin)
-		{
-			__result = GetSprite(__result, "head", EnumCache<SkinType>.GetName(skin));
-		}
-
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(TribeData.Type) })]
-		private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, TribeData.Type type)
-		{
-			__result = GetSprite(__result, "head", EnumCache<TribeData.Type>.GetName(type));
-		}
-
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(SpriteData.SpecialFaceIcon) })]
-		private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, SpriteData.SpecialFaceIcon specialId)
-		{
-			__result = GetSprite(__result, "head", specialId.ToString());
-		}
-
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetAvatarPartSpriteAddress))]
-		private static void SpriteData_GetAvatarPartSpriteAddress(ref SpriteAddress __result, string sprite)
-		{
-			__result = GetSprite(__result, sprite, "");
-		}
-
-		// [HarmonyPostfix]
-		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHouseAddresses), new Type[] { typeof(int), typeof(string), typeof(SkinType) })]
-		// private static void SpriteData_GetHouseAddresses(ref Il2CppReferenceArray<SpriteAddress> __result, int type, string styleId, SkinType skinType)
-		// {
-		// 	List<SpriteAddress> sprites = new()
-		// 	{
-		// 		GetSprite(__result[0], "house", styleId, type)
-		// 	};
-		// 	if (skinType != SkinType.Default)
-		// 	{
-		// 		sprites.Add(GetSprite(__result[1], "house", EnumCache<SkinType>.GetName(skinType), type));
-		// 	}
-		// 	__result = sprites.ToArray();
-		// }
-
-		[HarmonyPrefix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetAddress))]
-		public static bool SpriteData_GetAddress(ref SpriteAddress __result, PickerType pickerType, string skinID)
-		{
-			switch (pickerType)
-			{
-				case PickerType.Head:
-					__result = SpriteData.GetHeadSpriteAddress(skinID);
-					break;
-				case PickerType.Unit:
-					__result = new SpriteAddress("Units", skinID);
-					break;
-				case PickerType.TerrainFeature:
-					__result = new SpriteAddress("TerrainFeatures", skinID);
-					break;
-				case PickerType.Roof:
-					__result = new SpriteAddress("Units", "roof_" + skinID);
-					break;
-				case PickerType.PolytaurHead:
-					__result = new SpriteAddress("Units", "polytaur_2_" + skinID);
-					break;
-				case PickerType.Animal:
-					__result = SpriteData.GetResourceSpriteAddress(ResourceData.Type.Game, skinID);
-					break;
-				case PickerType.Fruit:
-					__result = SpriteData.GetResourceSpriteAddress(ResourceData.Type.Fruit, skinID);
-					break;
-				case PickerType.Forest:
-					__result = SpriteData.GetTileSpriteAddress(Polytopia.Data.TerrainData.Type.Forest, skinID);
-					break;
-				case PickerType.Mountain:
-					__result = SpriteData.GetTileSpriteAddress(Polytopia.Data.TerrainData.Type.Mountain, skinID);
-					break;
-				case PickerType.Monument_1:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument1, skinID);
-					break;
-				case PickerType.Monument_2:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument2, skinID);
-					break;
-				case PickerType.Monument_3:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument3, skinID);
-					break;
-				case PickerType.Monument_4:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument4, skinID);
-					break;
-				case PickerType.Monument_5:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument5, skinID);
-					break;
-				case PickerType.Monument_6:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument6, skinID);
-					break;
-				case PickerType.Monument_7:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument7, skinID);
-					break;
-			}
-			return false;
 		}
 
 		[HarmonyPrefix]
@@ -179,7 +48,8 @@ namespace PolyMod
 		private static void City_UpdateObject(City __instance)
 		{
 			Console.Write((int)__instance.Owner.tribe);
-			if (__instance.Owner != null && (int)__instance.Owner.tribe > 17){
+			if (__instance.Owner != null && (int)__instance.Owner.tribe > 17)
+			{
 				__instance.cityRenderer.Tribe = TribeData.Type.Imperius;
 				__instance.cityRenderer.SkinType = SkinType.Default;
 				__instance.cityRenderer.RefreshCity();
@@ -214,7 +84,6 @@ namespace PolyMod
 		[HarmonyPatch(typeof(Unit), nameof(Unit.SetVisible))]
 		private static void Unit_SetVisible(Unit __instance, bool isVisible)
 		{
-			//Console.Write("Parent: " + __instance.transform.parent + ", of unit with type: " + __instance.UnitData.type);
 			Transform unitTransform = new Transform();
 			foreach (Transform unit in __instance.transform.parent)
 			{
@@ -226,13 +95,17 @@ namespace PolyMod
 				GameObject spriteContainer = spriteContainerTransform.gameObject;
 				Transform headTransform = spriteContainer.transform.Find("Head");
 				Transform bodyTransform = spriteContainer.transform.Find("Body");
-				if(headTransform != null){
+				if (headTransform != null)
+				{
 					SpriteRenderer sr = headTransform.gameObject.GetComponent<SpriteRenderer>();
-					if(sr != null){
-						foreach (var kvp in _sprites) {
+					if (sr != null)
+					{
+						foreach (var kvp in _sprites)
+						{
 							if (kvp.Value) Console.WriteLine(kvp.Key);
 						}
-						if(sr.sprite.name == "head" || sr.sprite.name == ""){
+						if (sr.sprite.name == "head" || sr.sprite.name == "")
+						{
 							string idKey = gldDictionary.FirstOrDefault(x => x.Value == (int)__instance.Owner.tribe).Key;
 							string spritesKey = "head_" + idKey + "_";
 							Console.Write("Found custom tribe's head, changing sprite to: " + spritesKey + ".png");
@@ -240,7 +113,7 @@ namespace PolyMod
 						}
 					}
 				}
-				if(bodyTransform != null){}
+				if (bodyTransform != null) { }
 			}
 		}
 
@@ -258,7 +131,18 @@ namespace PolyMod
 
 					if (Path.GetExtension(name) == ".dll")
 					{
-						PolyscriptLoad(entry.ReadBytes());
+						try
+						{
+							Assembly assembly = Assembly.Load(entry.ReadBytes());
+							foreach (Type type in assembly.GetTypes())
+							{
+								type.GetMethod("Load")?.Invoke(null, null);
+							}
+						}
+						catch (Exception exception)
+						{
+							Plugin.logger.LogInfo(exception.Message);
+						}
 					}
 					if (Path.GetFileName(name) == "patch.json")
 					{
@@ -266,123 +150,84 @@ namespace PolyMod
 					}
 					if (Path.GetExtension(name) == ".png")
 					{
-						_textures.Add(name, entry.ReadBytes());
+						Vector2 pivot = Path.GetFileNameWithoutExtension(name).Split("_")[0] switch
+						{
+							"field" => new(0.5f, 0.0f),
+							"mountain" => new(0.5f, -0.375f),
+							_ => new(0.5f, 0.5f),
+						};
+						_sprites.Add(Path.GetFileNameWithoutExtension(name), Api.BuildSprite(entry.ReadBytes(), pivot));
 					}
 				}
 			}
 		}
 
-		public static void Load(JObject gameLogicdata)
+		public static void Load(JObject gld)
 		{
-			GameManager.GetSpriteAtlasManager().cachedSprites.TryAdd("Heads", new());
-			foreach (var patch in _patches){
+			foreach (var patch in _patches)
+			{
 				try
 				{
-					GameLogicDataPatch(gameLogicdata, patch);
-				} catch {}
-			}
-			foreach (var sprite_ in _textures){
-				Vector2 pivot = Path.GetFileNameWithoutExtension(sprite_.Key).Split("_")[0] switch
-				{
-					"field" => new(0.5f, 0.0f),
-					"mountain" => new(0.5f, -0.375f),
-					_ => new(0.5f, 0.5f),
-				};
-				Sprite sprite = Api.BuildSprite(sprite_.Value, pivot);
-				GameManager.GetSpriteAtlasManager().cachedSprites["Heads"].Add(Path.GetFileNameWithoutExtension(sprite_.Key), sprite);
-				_sprites.Add(Path.GetFileNameWithoutExtension(sprite_.Key), sprite);
-			}
-		}
-
-		private static void PolyscriptLoad(byte[] polyscriptData)
-		{
-			try{
-				Assembly assembly = Assembly.Load(polyscriptData);
-				foreach (Type type in assembly.GetTypes())
-				{
-					type.GetMethod("Load")?.Invoke(null, null);
-				}
-			}
-			catch(Exception exception){
-				Plugin.logger.LogInfo(exception.Message);
-			}
-		}
-		private static void GameLogicDataPatch(JObject gld, JObject patch)
-		{
-			foreach (JToken jtoken in patch.SelectTokens("$.localizationData.*").ToArray())
-			{
-				JArray token = jtoken.Cast<JArray>();
-				TermData term = LocalizationManager.Sources[0].AddTerm(Api.GetJTokenName(token).Replace('_', '.'));
-				List<string> strings = new();
-				for (int i = 0; i < token.Count; i++)
-				{
-					strings.Add((string)token[i]);
-				}
-				for (int i = 0; i < LocalizationManager.GetAllLanguages().Count - strings.Count; i++)
-				{
-					strings.Add(term.Term);
-				}
-				term.Languages = new Il2CppStringArray(strings.ToArray());
-			}
-			patch.Remove("localizationData");
-
-			foreach (JToken jtoken in patch.SelectTokens("$.*.*").ToArray())
-			{
-				JObject token = jtoken.Cast<JObject>();
-
-				if (token["idx"] != null && (int)token["idx"] == -1)
-				{
-					++_autoidx;
-					token["idx"] = _autoidx;
-					string id = Api.GetJTokenName(token);
-					gldDictionary[id] = _autoidx;
-					switch (Api.GetJTokenName(token, 2))
+					foreach (JToken jtoken in patch.SelectTokens("$.localizationData.*").ToArray())
 					{
-						case "tribeData":
-							EnumCache<TribeData.Type>.AddMapping(id, (TribeData.Type)_autoidx);
-							break;
-						case "terrainData":
-							EnumCache<Polytopia.Data.TerrainData.Type>.AddMapping(id, (Polytopia.Data.TerrainData.Type)_autoidx);
-							break;
-						case "resourceData":
-							EnumCache<ResourceData.Type>.AddMapping(id, (ResourceData.Type)_autoidx);
-							PrefabManager.resources.TryAdd((ResourceData.Type)_autoidx, PrefabManager.resources[ResourceData.Type.Game]);
-							break;
-						case "taskData":
-							EnumCache<TaskData.Type>.AddMapping(id, (TaskData.Type)_autoidx);
-							break;
-						case "improvementData":
-							EnumCache<ImprovementData.Type>.AddMapping(id, (ImprovementData.Type)_autoidx);
-							PrefabManager.improvements.TryAdd((ImprovementData.Type)_autoidx, PrefabManager.improvements[ImprovementData.Type.CustomsHouse]);
-							break;
-						case "unitData":
-							EnumCache<UnitData.Type>.AddMapping(id, (UnitData.Type)_autoidx);
-							PrefabManager.units.TryAdd((int)(UnitData.Type)_autoidx, PrefabManager.units[(int)UnitData.Type.Scout]);
-							break;
-						case "techData":
-							EnumCache<TechData.Type>.AddMapping(id, (TechData.Type)_autoidx);
-							break;
+						JArray token = jtoken.Cast<JArray>();
+						TermData term = LocalizationManager.Sources[0].AddTerm(Api.GetJTokenName(token).Replace('_', '.'));
+						List<string> strings = new();
+						for (int i = 0; i < token.Count; i++)
+						{
+							strings.Add((string)token[i]);
+						}
+						for (int i = 0; i < LocalizationManager.GetAllLanguages().Count - strings.Count; i++)
+						{
+							strings.Add(term.Term);
+						}
+						term.Languages = new Il2CppStringArray(strings.ToArray());
 					}
+					patch.Remove("localizationData");
+
+					foreach (JToken jtoken in patch.SelectTokens("$.*.*").ToArray())
+					{
+						JObject token = jtoken.Cast<JObject>();
+
+						if (token["idx"] != null && (int)token["idx"] == -1)
+						{
+							++_autoidx;
+							token["idx"] = _autoidx;
+							string id = Api.GetJTokenName(token);
+							gldDictionary[id] = _autoidx;
+							switch (Api.GetJTokenName(token, 2))
+							{
+								case "tribeData":
+									EnumCache<TribeData.Type>.AddMapping(id, (TribeData.Type)_autoidx);
+									break;
+								case "terrainData":
+									EnumCache<Polytopia.Data.TerrainData.Type>.AddMapping(id, (Polytopia.Data.TerrainData.Type)_autoidx);
+									break;
+								case "resourceData":
+									EnumCache<ResourceData.Type>.AddMapping(id, (ResourceData.Type)_autoidx);
+									PrefabManager.resources.TryAdd((ResourceData.Type)_autoidx, PrefabManager.resources[ResourceData.Type.Game]);
+									break;
+								case "taskData":
+									EnumCache<TaskData.Type>.AddMapping(id, (TaskData.Type)_autoidx);
+									break;
+								case "improvementData":
+									EnumCache<ImprovementData.Type>.AddMapping(id, (ImprovementData.Type)_autoidx);
+									PrefabManager.improvements.TryAdd((ImprovementData.Type)_autoidx, PrefabManager.improvements[ImprovementData.Type.CustomsHouse]);
+									break;
+								case "unitData":
+									EnumCache<UnitData.Type>.AddMapping(id, (UnitData.Type)_autoidx);
+									PrefabManager.units.TryAdd((int)(UnitData.Type)_autoidx, PrefabManager.units[(int)UnitData.Type.Scout]);
+									break;
+								case "techData":
+									EnumCache<TechData.Type>.AddMapping(id, (TechData.Type)_autoidx);
+									break;
+							}
+						}
+					}
+
+					gld.Merge(patch, Plugin.GLD_MERGE_SETTINGS);
 				}
-			}
-
-			gld.Merge(patch, Plugin.GLD_MERGE_SETTINGS);
-		}
-
-		private static SpriteAddress GetSprite(SpriteAddress sprite, string name, string style = "", int level = 0)
-		{
-			GetSpriteIfFound($"{name}__", ref sprite);
-			GetSpriteIfFound($"{name}_{style}_", ref sprite);
-			GetSpriteIfFound($"{name}__{level}", ref sprite);
-			GetSpriteIfFound($"{name}_{style}_{level}", ref sprite);
-			return sprite;
-		}
-
-		private static void GetSpriteIfFound(string id, ref SpriteAddress sprite)
-		{
-			if (GameManager.GetSpriteAtlasManager().cachedSprites["Heads"].TryGetValue(id, out _))
-			{
-				sprite = new SpriteAddress("Heads", id);
+				catch { }
 			}
 		}
 	}
