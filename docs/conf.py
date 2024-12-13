d063462f1d3480089ab08e5d84b949f83b0f29cb
IExploitableMan <pythonblack5000@gmail.com>
2024-12-14 00:32:48 +0300

    Remove RtD...

--- docs/conf.py
+++
@@ -1,8 +0,0 @@
-project = "PolyMod"
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-]
-templates_path = ["_templates"]
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
--- docs/index.rst
+++
@@ -1,2 +0,0 @@
-Welcome to PolyMod's documentation!
-===================================
