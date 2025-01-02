70678dbd8d1048b545d260218d50a2c30593d331
IExploitableMan <pythonblack5000@gmail.com>
2025-01-02 18:05:07 +0300

    Add `PolyMod.json` config

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Text.Json;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
@@ -10,17 +11,33 @@ namespace PolyMod
 	[BepInPlugin("com.polymod", "PolyMod", VERSION)]
 	public class Plugin : BepInEx.Unity.IL2CPP.BasePlugin
 	{
+		internal record PolyConfig(
+			bool debug = false
+		);
+
 		internal const string VERSION = "0.0.0";
 		internal const int AUTOIDX_STARTS_FROM = 1000;
 		internal static readonly string BASE_PATH = Path.Combine(BepInEx.Paths.BepInExRootPath, "..");
 		internal static readonly string MODS_PATH = Path.Combine(BASE_PATH, "Mods");
+		internal static readonly string CONFIG_PATH = Path.Combine(BASE_PATH, "PolyMod.json");
 
 #pragma warning disable CS8618
+		internal static PolyConfig config;
 		internal static ManualLogSource logger;
 #pragma warning restore CS8618
 
 		public override void Load()
 		{
+			try 
+			{
+				config = JsonSerializer.Deserialize<PolyConfig>(File.ReadAllText(CONFIG_PATH))!;
+			}
+			catch 
+			{
+				config = new();
+				File.WriteAllText(CONFIG_PATH, JsonSerializer.Serialize(config));
+			}
+
 			logger = Log;
 			ConfigFile.CoreConfig[new("Logging.Disk", "WriteUnityLog")].BoxedValue = true;
 			AudioClipLoader.Init();
